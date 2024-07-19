ec8f3636a247c1a4d8a2d20aa02a9de4444f290b
emily <vsreelasya04@gmail.com>
2024-07-19 18:42:31 +0530

    vercel deployment

---
+++ prisma/migrations/20240718072740_adding_relation/migration.sql
@@ -0,0 +1,17 @@
+/*
+  Warnings:
+
+  - Added the required column `name` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Todo" ADD COLUMN     "userEmail" TEXT;
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "name" TEXT NOT NULL;
+
+-- CreateIndex
+CREATE INDEX "Todo_userEmail_idx" ON "Todo"("userEmail");
+
+-- AddForeignKey
+ALTER TABLE "Todo" ADD CONSTRAINT "Todo_userEmail_fkey" FOREIGN KEY ("userEmail") REFERENCES "User"("email") ON DELETE SET NULL ON UPDATE CASCADE;
